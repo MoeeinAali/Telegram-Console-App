3ed9ac524d8389f66c4d6216d4b0ec39a77f34f6
Moeein <moeeeinaali@gmail.com>
2024-05-11 19:37:21 +0330

    Add Main.java Codes

--- Telegram-Console-App/src/Main.java
+++ Telegram-Console-App/src/Main.java
@@ -1,5 +1,30 @@
+import controller.Messenger;
+import view.LoginMenu;
+import view.MenuStatus;
+import view.MessengerMenu;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            switch (MenuStatus.getMenuStatus()) {
+                case LOGIN_MENU:
+                    LoginMenu loginMenu = new LoginMenu();
+                    loginMenu.run(scanner);
+                    break;
+                case MESSENGER_MENU:
+                    MessengerMenu messengerMenu = new MessengerMenu(Messenger.getCurrentUser());
+                    messengerMenu.run(scanner);
+                    break;
+                case CHAT_MENU:
+                    ChatMenu chatMenu = new ChatMenu(MessengerMenu.enteredChat, Messenger.getCurrentUser());
+                    chatMenu.run(scanner);
+                    break;
+                case TERMINATE:
+                    break;
+            }
+        }
     }
 }
